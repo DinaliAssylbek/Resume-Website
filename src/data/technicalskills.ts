5c47d39591560a5016ea3b014ed5bc623a3612ef
DinaliAssylbek <dinali.assylbek.2003@gmail.com>
2026-07-16 22:01:43 -0400

    Added technical skill

--- src/data/technicalskills.ts
+++ src/data/technicalskills.ts
@@ -5,5 +5,5 @@ export const technicalSkills : TechnicalSkills = {
     frameworks: ["Next.js", "Flutter", "React", "Flask", "FastAPI", ".NET"],
     cloud: ["AWS", "Azure", "Docker", "Firebase", "Railway", "Daytona", "Bio-T"],
     tools: ["CI/CD", "Git", "Yocto", "BitBucket", "CircleCI"],
-    etc: ["Linux", "Windows", "Algorithms", "Data Structures", "LLM Evaluation"]
+    etc: ["Linux", "Windows", "Algorithms", "Data Structures", "LLM Evaluation", "Signal Processing"]
 }
