8cf8c76af12916f84980652939540e42bf971f30
DinaliAssylbek <dinali.assylbek.2003@gmail.com>
2026-07-16 20:42:01 -0400

    Added all skills to ui

--- src/app/components/TechnicalSkills.tsx
+++ src/app/components/TechnicalSkills.tsx
@@ -1,15 +1,86 @@
 import React from 'react'
-import { FaGear } from "react-icons/fa6";
+import { VscGearCompact } from "react-icons/vsc";
+import { technicalSkills } from '@/data/technicalskills';
 
 const TechnicalSkills = () => {
   return (
     <section>
         <div className='flex gap-4'>
-            <FaGear className='text-2xl text-[var(--secondary)]'/>
+            <VscGearCompact className='text-2xl text-[var(--secondary)]'/>
             <h2 className='text-xl font-bold text-[var(--primary)]'>
                 Technical Skills
             </h2>
         </div>
+        <h3 className='text-sm text-[var(--neutral)] mb-3'>
+            LANGUAGES
+        </h3>
+        <ul className='flex flex-wrap gap-2 mb-3'>
+            {technicalSkills.languages.map((skills, index) => (
+                < li
+                    key={index}
+                    className="bg-[var(--secondary)]/15 px-3 py-1 text-sm font-medium text-[var(--primary)]"
+                    style={{ fontFamily: "var(--font-label)" }}
+                >
+                    {skills}
+                </li>
+            ))}
+        </ul> 
+        <h3 className='text-sm text-[var(--neutral)] mb-3'>
+            FRAMEWORKS
+        </h3>
+        <ul className='flex flex-wrap gap-2 mb-3'>
+            {technicalSkills.frameworks.map((skills, index) => (
+                < li
+                    key={index}
+                    className="bg-[var(--secondary)]/15 px-3 py-1 text-sm font-medium text-[var(--primary)]"
+                    style={{ fontFamily: "var(--font-label)" }}
+                >
+                    {skills}
+                </li>
+            ))}
+        </ul> 
+        <h3 className='text-sm text-[var(--neutral)] mb-3'>
+            CLOUD
+        </h3>
+        <ul className='flex flex-wrap gap-2 mb-3'>
+            {technicalSkills.cloud.map((skills, index) => (
+                < li
+                    key={index}
+                    className="bg-[var(--secondary)]/15 px-3 py-1 text-sm font-medium text-[var(--primary)]"
+                    style={{ fontFamily: "var(--font-label)" }}
+                >
+                    {skills}
+                </li>
+            ))}
+        </ul> 
+        <h3 className='text-sm text-[var(--neutral)] mb-3'>
+            TOOLS
+        </h3>
+        <ul className='flex flex-wrap gap-2 mb-3'>
+            {technicalSkills.tools.map((skills, index) => (
+                < li
+                    key={index}
+                    className="bg-[var(--secondary)]/15 px-3 py-1 text-sm font-medium text-[var(--primary)]"
+                    style={{ fontFamily: "var(--font-label)" }}
+                >
+                    {skills}
+                </li>
+            ))}
+        </ul> 
+        <h3 className='text-sm text-[var(--neutral)] mb-3'>
+            OTHER
+        </h3>
+        <ul className='flex flex-wrap gap-2 mb-3'>
+            {technicalSkills.etc.map((skills, index) => (
+                < li
+                    key={index}
+                    className="bg-[var(--secondary)]/15 px-3 py-1 text-sm font-medium text-[var(--primary)]"
+                    style={{ fontFamily: "var(--font-label)" }}
+                >
+                    {skills}
+                </li>
+            ))}
+        </ul> 
     </section>
   )
 }
