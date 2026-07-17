cf9c9c4e3dbd9165e33904a5cad4ab98faf98b85
DinaliAssylbek <dinali.assylbek.2003@gmail.com>
2026-07-16 20:18:26 -0400

    Polished Skills section

--- src/data/technicalskills.ts
+++ src/data/technicalskills.ts
@@ -1,9 +1,9 @@
 import { TechnicalSkills } from "@/types/technicalskills";
 
 export const technicalSkills : TechnicalSkills = {
-    languages:["Python", "C/C++", "Typescript", "Java", "Bash", "Powershell"],
-    frameworks: ["Flutter", "React", "Flask"],
-    cloud: ["AWS", "Docker", "Firebase"],
-    tools: [],
-    etc: []
+    languages:["Python", "C/C++","Typescript", "C#", "Bash", "Powershell"],
+    frameworks: ["Next.js", "Flutter", "React", "Flask", "FastAPI", ".NET"],
+    cloud: ["AWS", "Azure", "Docker", "Firebase", "Railway", "Daytona", "Bio-T"],
+    tools: ["CI/CD", "Git", "Yocto", "BitBucket", "CircleCI"],
+    etc: ["Linux", "Windows", "Algorithms", "Data Structures", "LLM Evaluation"]
 }
